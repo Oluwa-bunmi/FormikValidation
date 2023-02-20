bd2f1cbfdb7162fa36f52c5b95e56a660d94fdaf
Jummy007 <gbemisoyeadejumoke@gmail.com>
2023-02-20 04:19:35 +0100

    validation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import { BsPersonCircle } from "react-icons/bs";
+import { useFormik } from "formik";
+import { formSchema } from "./schemas";
+
 function App() {
+  const initialValues = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone_number: "",
+    password: "",
+    confirm_password:""
+  };
+    const onSubmit = async (payload, actions) => {
+      console.log(payload);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      actions.resetForm();
+    };
+      const {
+        values,
+        touched,
+        handleChange,
+        handleBlur,
+        errors,
+        handleSubmit,
+        isSubmitting,
+      } = useFormik({
+        initialValues,
+        validationSchema: formSchema,
+        onSubmit,
+      });
   return (
     <div className="App flex justify-center items-center h-screen  bg-[#9633ff]">
-      <form className="flex justify-center gap-6 flex-col rounded-lg border border-white px-16 py-5 bg-white w-[40%]">
+      <form onSubmit={handleSubmit} className="flex justify-center gap-6 flex-col rounded-lg border border-white px-16 py-5 bg-white w-[40%]">
         <h1 className="text-3xl text-center text-[#9633ff] m-4">FP</h1>
         <div className="flex justify-between w-full gap-4">
           <div className="flex items-center w-full gap-2 border-2 border-gray-300 p-2 rounded-md">
@@ -12,6 +41,7 @@ function App() {
             <input
               type="text"
               placeholder="Your firstname"
+              name="first_name"
               id="first_name"
               className="outline-none w-full "
             />
@@ -23,6 +53,7 @@ function App() {
             <input
               type="text"
               placeholder="Your lastname"
+              name="last_name"
               id="last_name"
               className="outline-none w-full "
             />
@@ -36,18 +67,20 @@ function App() {
             type="text"
             placeholder="Enter email"
             id="email"
+            name="email"
             className="outline-none w-full"
           />
         </div>
 
         <div className="h-[54px] flex items-center w-full gap-2 border-2 border-gray-300 p-2 rounded-md">
-          <label htmlFor="password">
+          <label htmlFor="phone_number">
             <BsPersonCircle size={18} color="#9633ff" />
           </label>
           <input
             type="number"
             placeholder="Phone number"
             id="phone_number"
+            name="phone_number"
             className="outline-none w-full "
           />
         </div>
@@ -59,22 +92,25 @@ function App() {
             type="password"
             placeholder="Enter password"
             id="password"
+            name="password"
             className="outline-none w-full "
           />
         </div>
         <div className="h-[54px] flex items-center w-full gap-2 border-2 border-gray-300 p-2 rounded-md">
-          <label htmlFor="password">
+          <label htmlFor="confirm_password">
             <BsPersonCircle size={18} color="#9633ff" />
           </label>
           <input
             type="password"
             placeholder="Re-enter password"
-            id="password"
+            id="confirm_password"
+            name="confirm_password"
             className="outline-none w-full "
           />
         </div>
         <button
           type="submit"
+          disabled={isSubmitting}
           className="w-full bg-[#9633ff] h-[54px] text-white rounded-lg outline-none"
         >
           SIGN IN
